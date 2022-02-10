8677c1df9d2e96d413367652e6d0dbf22db052d2
Solaumein <94980129+Solaumein@users.noreply.github.com>
2022-02-10 10:46:34 +0100

    Update tsock_v0.c

--- tsock_v0.c
+++ tsock_v0.c
@@ -18,18 +18,36 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
+void construire_message(char *message, char motif, int lg) {
+  int i;
+  for (i=0;i<lg;i++) message[i] = motif;
+}
+  
+void afficher_message(char *message, int lg) {
+    int i;
+    printf("message construit : ");
+    for (i=0;i<lg;i++) printf("%c", message[i]); printf("\n");
+}
+
+
+
 void main (int argc, char **argv)
 {
 	int c;
 	extern char *optarg;
 	extern int optind;
+	char *hostname= "";
+	int lg = 0;
+	struct hostent *host;
+	int sock;
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
-	while ((c = getopt(argc, argv, "pn:s")) != -1) {
+	int udp = 0;
+	while ((c = getopt(argc, argv, "pn:su")) != -1) {
 		switch (c) {
 		case 'p':
 			if (source == 1) {
-				printf("usage: cmd [-p|-s][-n ##]\n");
+				printf("Puit\nusage: cmd [-p|-s][-n ##]\n");
 				exit(1);
 			}
 			source = 0;
@@ -37,7 +55,7 @@ void main (int argc, char **argv)
 
 		case 's':
 			if (source == 0) {
-				printf("usage: cmd [-p|-s][-n ##]\n");
+				printf("Source\nusage: cmd [-p|-s][-n ##]\n");
 				exit(1) ;
 			}
 			source = 1;
@@ -46,20 +64,59 @@ void main (int argc, char **argv)
 		case 'n':
 			nb_message = atoi(optarg);
 			break;
+		case 'u':
+		  udp = 1;
+		  
+		  // UDP
+		  
+		  break;
+		  
 
 		default:
-			printf("usage: cmd [-p|-s][-n ##]\n");
+			printf("Option invalide\nusage: cmd [-p|-s][-n ##]\n");
 			break;
 		}
 	}
 
+	if (udp==1){
+	  sock = socket(AF_INET,SOCK_DGRAM,0);
+	  if (sock == -1){printf("echec creation sock");
+	  
+
+	  }
+	}else {printf("C'est pas de l'udp\n");}
+
+	if(source == 1){
+	 hostname = argv[argc-2];
+	}
+        int port = atoi(argv[argc-1]);
+	port = htons(port);
+	
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = port;
+
+	if(hostname!=""){
+	  if((host=gethostbyname(hostname))==NULL){
+	    printf("erreur sur gethostname\n");
+	    exit(1);
+	  }
+	  memcpy((char*)&(addr.sin_addr.s_addr),host->h_addr,host->h_length);
+	}else{
+	  //local
+	}
+	
 	if (source == -1) {
 		printf("usage: cmd [-p|-s][-n ##]\n");
 		exit(1) ;
 	}
 
-	if (source == 1)
+	if (source == 1){
 		printf("on est dans le source\n");
+		if((sendto(sock,"",lg,0,(struct sockaddr *)&addr,sizeof(addr)))==-1){printf("Erreur envoi message\n");}
+		else {printf("message envoye\n");}
+	}
+	
 	else
 		printf("on est dans le puits\n");
 
@@ -77,4 +134,3 @@ void main (int argc, char **argv)
 
 	}
 }
-
